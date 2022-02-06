d7f436d850debff3808829867f0ca834043134f4
Paloma Queiroz <palomacqueiroz@hotmail.com>
2022-02-06 01:27:26 -0300

    ex.final

---
+++ PooGuanabara/src/pooguanabara_aula14_exfinal/Video.java
@@ -0,0 +1,45 @@
+package pooguanabara_aula14_exfinal;
+
+public class Video {
+    protected String titulo;
+    protected int avaliacao;
+    protected int views;
+    protected int curtidas;
+    protected boolean reproduzindo;
+    
+
+    //Getters and Setters.
+    public String getTitulo() {
+        return titulo;
+    }
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+    public int getViews() {
+        return views;
+    }
+    public void setViews(int views) {
+        this.views = views;
+    }
+    public int getCurtidas() {
+        return curtidas;
+    }
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+    public boolean getReproduzindo() {
+        return reproduzindo;
+    }
+    public void setReproduzindo(boolean reproduzindo) {
+        this.reproduzindo = reproduzindo;
+    }
+
+
+
+}
